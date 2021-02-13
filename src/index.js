def8d74826e045106e86578969a1c9723cd5157c
GeorgeFreeman <thenewsit@gmail.com>
2021-02-13 12:51:15 +0500

    init

---
+++ src/index.js
@@ -0,0 +1,7 @@
+export const staticTokens = {
+  clientName: { placeholder: 'Имя клиента', field: 'name' },
+  clientSurname: { placeholder: 'Фамилия клиента', field: 'surname' },
+  clientCompany: { placeholder: 'Компания клиента', field: 'company' },
+  clientEmail: { placeholder: 'Email клиента', field: 'email' },
+  clientPhone: { placeholder: 'Телефон клиента', field: 'phone' }
+}
